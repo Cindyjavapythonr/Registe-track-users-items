5703df428dea10708ea81b9237ec0f5bcf2366ef
KahMeng2 <kahmeng.lee12@gmail.com>
2023-05-16 22:24:25 +1000

    Added new code to about us page

--- src/mainpagefunctions/aboutus.js
+++ src/mainpagefunctions/aboutus.js
@@ -1,13 +1,14 @@
+import './AboutMe.css';
 
 export const AboutUs = () => {
     // A static page that shows briefly mentions the history, achievements, and the philanthropic activities of the organization. The UI should support different content types such as text, images, audio and video files.
     // Two buttons one links to the facebook page one links insta.
-    function handleClick(){
+    
 
-    }
     return (
         <div className="about-me">
-        <h1>Our Organization VRIFamily Charity Aid</h1>
+            <img src="https://i.imgur.com/6hGSXKj.jpg" alt="Organization's Logo" width="200" height="200" style= {{paddingBottom: '100' }} />
+        <h1 className= "title">Our Organization: VRIFamily Charity Aid</h1>
         <p>
         VR1Family Charity Aid Services started off as a small not for profit organization, founded in June 2019 by a group of 5 volunteers in a garage in Mallacoota, Australia.
         Our Focus is to provide immediate humanitarian assistance and improve the lives of people facing economic, social, and health challenges in the immediate aftermath of natural calamities in their local community. </p>
@@ -23,17 +24,22 @@ export const AboutUs = () => {
             Since 2020, VR1Family slowly started1 expanding their footprint outside Australia, serving in multiple humanitarian aid assistance programs such as Mount Sumeru volcanic eruption in Indonesia
             , flooding in Belgium and Germany , and the earthquake in Haiti.
         </p>
+        <h3 className='title'>Follow Us</h3>
         <div className="media">
-            <img src="https://dummyimage.com/200x200/000/fff" alt="Organization's Logo" />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/bdBG5VO01e0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <div className="buttons">
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <a href=" https://www.facebook.com/VR1Family" target="_blank" rel="noopener noreferrer">
                 <button>Facebook</button>
                 </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/vr1familycharity/" target="_blank" rel="noopener noreferrer">
                 <button>Instagram</button>
                 </a>
             </div>
+            <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=645f04220c19c90019685b11&product=inline-follow-buttons&source=platform" async="async"></script>
+            <div class="sharethis-inline-follow-buttons"></div>
+
         </div>
+        
         </div>
     )
 };
